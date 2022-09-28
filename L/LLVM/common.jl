422255dd824dfb309ed5a1ee1176d97236dae6a6
Valentin Churavy <v.churavy@gmail.com>
2022-09-28 14:57:13 -0400

    Build LLVM 15 for the first time

--- L/LLVM/common.jl
+++ L/LLVM/common.jl
@@ -15,6 +15,7 @@ const llvm_tags = Dict(
     v"12.0.1" => "980d2f60a8524c5546397db9e8bbb7d6ea56c1b7", # julia-12.0.1-4
     v"13.0.1" => "8a2ae8c8064a0544814c6fac7dd0c4a9aa29a7e6", # julia-13.0.1-3
     v"14.0.6" => "63feb57573d16e7c64d99c971659dd98971ad06c", # julia-14.0.6-0
+    v"15.0.1" => "dccf0cc8852cd13d6f479afa5513db07bc586732", # julia-15.0.1-0
 )
 
 const buildscript = raw"""
